e956fbdd204ee4e387b5e2f987eb6117d887a217
Alexander Angelov <stu2001321071@uni-plovdiv.bg>
2021-10-27 20:39:26 +0300

    First Upload

---
+++ Inheritance - Exercise/Person/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        public Person(string name,int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Name: {Name}, Age: {Age}");
+            return sb.ToString() ; 
+        }
+    }
+}
